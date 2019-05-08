7e53b890eccea1f86ba6a3b93f607558d466df32
admin <zhangyule1993@sina.com>
2019-05-09 00:41:53 +0800

    用户服务

---
+++ user-thrift-service/src/main/java/com/biubiu/user/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.biubiu.user.service;
+
+/**
+ * @author yule.zhang
+ * @date 2019/5/9 0:32
+ * @email zhangyule1993@sina.com
+ * @description 实现用户服务
+ */
+public class UserServiceImpl {
+}
